5c5ae8aaccca38e6197e1f18d481344de58b6fa8
Fabio Rocha <frock81@yahoo.com.br>
2023-07-05 10:36:04 -0300

    Trata inserção na árbore de busca binária

--- python/dsa/binary_search_tree.py
+++ python/dsa/binary_search_tree.py
@@ -2,6 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
+from typing import Any, Callable
+
+
 class BinarySearchTreeNode:
     
     def __init__(self,
@@ -9,27 +12,80 @@ class BinarySearchTreeNode:
                  left_value: int = None,
                  right_value: int = None) -> None:
         self._value = value
-        self.left_child: 'BinarySearchTreeNode' = None
-        self.right_child: 'BinarySearchTreeNode' = None
+        self._left_child: 'BinarySearchTreeNode' = None
+        self._right_child: 'BinarySearchTreeNode' = None
         if left_value is not None:
-            self.left_child = BinarySearchTreeNode(value=left_value)
+            self._left_child = BinarySearchTreeNode(value=left_value)
         if right_value is not None:
-            self.right_child = BinarySearchTreeNode(value=right_value)
+            self._right_child = BinarySearchTreeNode(value=right_value)
         
     def get_value(self) -> int:
         return self._value
     
-    def set_left_child(self, value: int) -> None:
-        self.left_child = BinarySearchTreeNode(value=value)
+    def set_left_child(self, bst_node: 'BinarySearchTreeNode') -> None:
+        self._left_child = bst_node
+        
+    def get_left_child(self) -> 'BinarySearchTreeNode':
+        return self._left_child
     
-    def set_right_child(self, value: int) -> None:
-        self.right_child = BinarySearchTreeNode(value=value)
+    def set_left_child_by_value(self, value: int) -> None:
+        self._left_child = BinarySearchTreeNode(value=value)
+
+    def get_left_child_value(self) -> int|None:
+        if self._left_child is not None:
+            return self._left_child.get_value()
+        return None
+    
+    def set_right_child(self, bst_node: 'BinarySearchTreeNode') -> None:
+        self._right_child = bst_node
+        
+    def get_right_child(self) -> 'BinarySearchTreeNode':
+        return self._right_child
+    
+    def set_right_child_by_value(self, value: int) -> None:
+        self._right_child = BinarySearchTreeNode(value=value)
+
+    def get_right_child_value(self) -> int|None:
+        if self._right_child is not None:
+            return self._right_child.get_value()
+        return None
+        
+    def traverse(self, callback_function: Callable):
+        callback_function(self)
+        if self._left_child is not None:
+            self._left_child.traverse(callback_function)
+        if self._right_child is not None:
+            self._right_child.traverse(callback_function)
+            
+    def insert(self, insert_node: 'BinarySearchTreeNode') -> None:
+        insert_value = insert_node.get_value()
+        if insert_value == self._value:
+            return
+        if insert_value < self._value:
+            left_child = self.get_left_child()
+            if left_child is None:
+                self.set_left_child(insert_node)
+                return
+            left_child.insert(insert_node)
+        if insert_value > self._value:
+            right_child = self.get_right_child()
+            if right_child is None:
+                self.set_right_child(insert_node)
+                return
+            right_child.insert(insert_node)
     
     def __repr__(self) -> str:
         return 'BinarySearchTreeNode()'
     
     def __str__(self) -> str:
-        return str(self.get_value())
+        left_value: str = ''
+        if self._left_child is not None:
+            left_value = str(self._left_child.get_value())
+        right_value: str = ''
+        if self._right_child is not None:
+            right_value = str(self._right_child.get_value())
+        return str(f'node: "{self.get_value()}" ( left: "{left_value}" , '
+                   f'right: "{right_value}" )')
 
 class BinarySearchTree:
     
@@ -40,3 +96,15 @@ class BinarySearchTree:
         if self._root_node is None:
             return True
         return False
+    
+    def insert(self, value: int) -> bool:
+        insert_node = BinarySearchTreeNode(value)
+        if self.is_empty():
+            self._root_node = insert_node
+            return
+        self._root_node.insert(insert_node)
+
+    def traverse(self, callback_function: Callable):
+        if self.is_empty():
+            return
+        self._root_node.traverse(callback_function)
--- python/tests/test_binary_search_tree.py
+++ python/tests/test_binary_search_tree.py
@@ -2,8 +2,9 @@
 # -*- coding: utf-8 -*-
 
 
-import sys
+import random
 import os
+import sys
 
 
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + f'{os.sep}..')
@@ -16,6 +17,30 @@ def test_creation_and_initialization():
     bstNode = BinarySearchTreeNode(0)
     bstNode = BinarySearchTreeNode(0,1)
     bstNode = BinarySearchTreeNode(0,1,2)
+    
+
+def test_insertion(bst: BinarySearchTree):
+    print('Insert 2')
+    bst.insert(2)
+    print('Insert 1')
+    bst.insert(1)
+    print('Insert 3')
+    bst.insert(3)
+    random.seed(10)
+    for _ in range(17):
+        random_value = random.randint(0,20)
+        print(f'Insert {random_value}')
+        bst.insert(random_value)
+
+
+def test_traverse():
+    bst = BinarySearchTree()
+    print("Empty BST:")
+    bst.traverse(print)
+    test_insertion(bst)
+    print("\nNot empty BST:")
+    bst.traverse(print)
 
 if __name__ == '__main__':
-    pass
+    test_creation_and_initialization()
+    test_traverse()
