17f22f0ae3f06f943ba4e42503868cc55ccab1d3
Fabio Rocha <frock81@yahoo.com.br>
2023-07-05 11:57:30 -0300

    Melhora o teste na árvore de busca binária

--- python/tests/test_binary_search_tree.py
+++ python/tests/test_binary_search_tree.py
@@ -10,6 +10,23 @@ import sys
 sys.path.append(os.path.dirname(os.path.abspath(__file__)) + f'{os.sep}..')
 
 from dsa.binary_search_tree import BinarySearchTree, BinarySearchTreeNode
+from typing import List
+
+SEED = 10
+SAMPLES = 100
+
+
+def get_inclusion_and_excluded_lists():
+    random.seed(SEED)
+    insertion_list = []
+    all_set = set()
+    for i in range(SAMPLES):
+        # Allow duplicates
+        insertion_list.append(random.randint(0,SAMPLES))
+        all_set.add(i)
+    excluded_list = list(all_set - set(insertion_list))
+    return insertion_list, excluded_list
+        
 
 
 def test_creation_and_initialization():
@@ -18,52 +35,34 @@ def test_creation_and_initialization():
     bstNode = BinarySearchTreeNode(0,1)
     bstNode = BinarySearchTreeNode(0,1,2)
     
-def test_search(bst: BinarySearchTree):
-    assert bst.contains(0)
-    assert bst.contains(1)
-    assert bst.contains(2)
-    assert bst.contains(3)
-    assert bst.contains(5)
-    assert bst.contains(6)
-    assert bst.contains(8)
-    assert bst.contains(10)
-    assert bst.contains(13)
-    assert bst.contains(14)
-    assert bst.contains(15)
-    assert bst.contains(16)
-    assert bst.contains(18)
-    assert bst.contains(20)
-    assert not bst.contains(4)
-    assert not bst.contains(7)
-    assert not bst.contains(9)
-    assert not bst.contains(11)
-    assert not bst.contains(12)
-    assert not bst.contains(17)
-    assert not bst.contains(19)
-    assert not bst.contains(500)
+def test_search(bst: BinarySearchTree,
+                included_list: list[int],
+                excluded_list: list[int]):
+    for included in included_list:
+        assert bst.contains(included)
+    for excluded in excluded_list:
+        assert not bst.contains(excluded)
     print("Search test passed!")
 
-def test_insertion(bst: BinarySearchTree):
-    print('Insert 2')
-    bst.insert(2)
-    print('Insert 1')
-    bst.insert(1)
-    print('Insert 3')
-    bst.insert(3)
-    random.seed(10)
-    for _ in range(17):
-        random_value = random.randint(0,20)
-        print(f'Insert {random_value}')
-        bst.insert(random_value)
+def test_insertion(bst: BinarySearchTree, insertion_list: list[int]):
+    for i in insertion_list:
+        bst.insert(i)
+    
+def test_traverse(bst: BinarySearchTree):
+    bst.traverse(print)
+    
 
 def test_operations():
     bst = BinarySearchTree()
     print("Empty BST:")
-    bst.traverse(print)
-    test_insertion(bst)
-    test_search(bst)
+    test_traverse(bst)
+    insertion_list, excluded_list = get_inclusion_and_excluded_lists()
+    test_insertion(bst=bst, insertion_list=insertion_list)
     print("\nNot empty BST:")
-    bst.traverse(print)
+    test_traverse(bst)
+    test_search(bst=bst,
+                included_list=list(set(insertion_list)),
+                excluded_list=excluded_list)
 
 if __name__ == '__main__':
     test_creation_and_initialization()
