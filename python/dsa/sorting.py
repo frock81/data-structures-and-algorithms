407697741067a620167e505745ece8bcb791a1fc
Fabio Rocha <frock81@yahoo.com.br>
2023-07-12 18:32:17 -0300

    Boilerplate insertion sort

---
+++ python/dsa/sorting.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from typing import List
+
+
+def insertion_sort(sorting_list: List) -> List:
+    return sorting_list
---
+++ python/tests/test_sorting.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
+from typing import Tuple
+import random
+import os
+import sys
+
+
+sys.path.append(os.path.dirname(os.path.abspath(__file__)) + f'{os.sep}..')
+
+
+# from dsa.sorting import bubble_sort, selection_sort, insertion_sort, merge_sort, quick_sort, heap_sort
+from dsa.sorting import insertion_sort
+
+
+def test_insertion_sort():
+    assert False
+
+
+if __name__ == '__main__':
+    # Test bubble sort
+    # Test selection sort
+    # Test insertion sort
+    test_insertion_sort()
+    # Test merge sort
+    # Test quick sort
+    # Test heap
+    # Test radix
